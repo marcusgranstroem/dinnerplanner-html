9dbbdd8ef330d923de4b1f482978f975cad9deb0
Nackey <nbhat@kth.se>
2018-02-13 14:39:44 +0100

    Added checkout controller

--- js/controllers/checkoutViewController.js
+++ js/controllers/checkoutViewController.js
@@ -1,1 +1,7 @@
-var CheckoutViewController = function(generalController, container, model) {}
+var CheckoutViewController = function(generalController, container, model) {
+    var backAndEditBtn = container.find('#back-and-edit').get(0);
+    var backAndEdit = function(evt) {
+        generalController.showMainPage();
+    }
+    backAndEditBtn.addEventListener("click", backAndEdit, false);
+}
