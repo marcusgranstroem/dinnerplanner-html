5099531aa4a8c360e5060cd2a7972f63b8135f83
Marcus Granström <mgranst@kth.se>
2018-02-13 21:30:54 +0100

    Added dynamic content to dishView.

--- js/view/dishView.js
+++ js/view/dishView.js
@@ -9,45 +9,32 @@ var DishView = function (container, model) {
     htmlContent += "<div id=\"dish-more\">";
     htmlContent += "<div id=\"dish-description\">";
     htmlContent += "<h2 id=\"dish-name\"></h2>";
-    htmlContent += "<img id=\"dish-picture\">";
-    htmlContent += "<p>Some text here. That's a tasty looking lasagna right there. It looks a little cold though, but I can't really place my finger on why. </p>";
-    htmlContent += "<button id=\"back-to-search\" class=\"button button2 small-button\">back to search </button>";
+    htmlContent += "<img id=\"dish-image\">";
+    htmlContent += "<p id=\"dish-info\"></p>";
+    htmlContent += "<button id=\"back-to-search\" class=\"button button2 small-button\">Back to search </button>";
     htmlContent += "</div>";
     htmlContent += "<div id=\"ingredients-list\">";
-    htmlContent += "<h2 class=\"text-margin-1\">INGREDIENTS FOR 3 PEOPLE </h2>";
+    htmlContent += "<h2 id=\"ingredients-header\" class=\"text-margin-1\"></h2>";
     htmlContent += "<hr>";
     htmlContent += "<!--Add ingredients here-->"; // Should be generated from model
     htmlContent += "<div class=\"ingredient-box\">";
-    htmlContent += "<div class=\"ingredient-column-amount\">";
-    htmlContent += "<p class=\"ingredient-text-\">2 tsp </p>";
-    htmlContent += "<p class=\"ingredient-text1\">70000 mg </p>";
-    htmlContent += "</div>";
-    htmlContent += "<div class=\"ingredient-column-description\">";
-    htmlContent += "<p class=\"ingredient-text2\">olive oil </p>";
-    htmlContent += "<p class=\"ingredient-text2\">fine grain baracuda oil </p>";
-    htmlContent += "</div>";
-    htmlContent += "<div class=\"ingredient-column-currency\">";
-    htmlContent += "<p class=\"ingredient-text3\">SEK </p>";
-    htmlContent += "<p class=\"ingredient-text3\">SEK </p>";
-    htmlContent += "</div>";
-    htmlContent += "<div class=\"ingredient-column-price\">";
-    htmlContent += "<p class=\"ingredient-text4\">15.00 </p>";
-    htmlContent += "<p class=\"ingredient-text4\">15000.00 </p>";
-    htmlContent += "</div>";
+    htmlContent += "<div id=\"ingredient-column-amount\"></div>";
+    htmlContent += "<div id=\"ingredient-column-description\"></div>";
+    htmlContent += "<div id=\"ingredient-column-currency\"></div>";
+    htmlContent += "<div id=\"ingredient-column-price\"></div>";
     htmlContent += "</div>";
     htmlContent += "<hr>";
     htmlContent += "<div id=\"ingredients-total\">";
-    htmlContent += "<a href=\"main_with_dish.html\">";
     htmlContent += "<button id=\"add-to-menu\"class=\"button button2 small-button\">Add to menu";
     htmlContent += "</button>";
-    htmlContent += "</a>";
-    htmlContent += "<p>SEK 77.20 </p>";
+    htmlContent += "<p id=\"tot-price\"></p>";
     htmlContent += "</div>";
     htmlContent += "</div>";
     htmlContent += "</div>";
     htmlContent += "<div id=\"dish-preperation\">";
     htmlContent += "<h2>PREPERATION";
     htmlContent += "</h2>";
+    htmlContent += "<p id=\"dish-prep\"></p>";
     htmlContent += "</div>";
 
     container.append(htmlContent);
@@ -60,7 +47,7 @@ var DishView = function (container, model) {
         return container.find('#back-to-search').get(0);
     }
 
-    this.update = function(model, changeDetails) {
+    var update = function(model, changeDetails) {
 	switch(changeDetails) {
 	case "dish_chosen":
 	    currentDish = model.getChosenDish();
@@ -68,9 +55,36 @@ var DishView = function (container, model) {
 	    break;
 	}
     }
-
+    model.addObserver(update);
+    
     var updateHTML = function() {
+	var guests = model.getNumberOfGuests();
 	container.find("#dish-name").html(currentDish.name);
+	container.find("#dish-image").attr({"src" : currentDish.image, "alt" : currentDish.name});
+	container.find("#dish-info").html(currentDish.info);
+	container.find("#ingredients-header").html("INGREDIENTS FOR " + guests + " PEOPLE");
+
+	// Generate ingredients for selected dish
+	var amount = "";
+	var descr = "";
+	var currency = "";
+	var price = "";
+	var totalPrice = 0;
+	currentDish.ingredients.forEach(function(ingredient) {
+	    var ingredientPrice = ingredient.price * guests;
+	    amount += "<p class=\"ingredient-text1\">" + (ingredient.quantity * guests) + " " + ingredient.unit + "</p>";
+	    descr += "<p class=\"ingredient-text2\">" + ingredient.name + "</p>";
+	    currency += "<p class=\"ingredient-text3\">" + "SEK" + "</p>";
+	    price += "<p class=\"ingredient-text4\">" + ingredientPrice + "</p>";
+	    totalPrice += ingredientPrice;
+	});
+
+	container.find("#ingredient-column-amount").html(amount);
+	container.find("#ingredient-column-description").html(descr);
+	container.find("#ingredient-column-currency").html(currency);
+	container.find("#ingredient-column-price").html(price);
+	container.find("#tot-price").html(totalPrice + " SEK");
+	container.find("#dish-prep").html(currentDish.description);
     }
 }
 
