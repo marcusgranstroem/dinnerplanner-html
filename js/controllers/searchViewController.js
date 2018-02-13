14b936ae12b6545c0c438d8d45be7ef1c35fc92f
Nackey <nbhat@kth.se>
2018-02-13 20:27:09 +0100

    searchcontroller less buggy

--- js/controllers/searchViewController.js
+++ js/controllers/searchViewController.js
@@ -1,8 +1,10 @@
-var SearchViewController = function(generalController, container, model) {
+var SearchViewController = function(generalController, view, model) {
     var searchBtn = view.getSearchBtn();
     var search = function(evt) {
         var selection = view.getSelection();
         var filter = view.getSearchField();
+        console.log(selection);
+        console.log(filter);
         model.makeSearch(selection, filter);
         model.notifyObservers("made_search");
     }
