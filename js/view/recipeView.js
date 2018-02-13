b6feae357a38a00aab60ec90b0ec81ac66ec869e
Nackey <nbhat@kth.se>
2018-02-13 21:19:56 +0100

    added update to recipeview

--- js/view/recipeView.js
+++ js/view/recipeView.js
@@ -1,24 +1,36 @@
-var RecipeView = function (container, model) {
-    
-    var htmlContent = "";
-    var dishes = model.getFullMenu();
-    var numberOfGuests = model.getNumberOfGuests();
-    
-    
-  
-    for (var i = 0; i < dishes.length; i++) {
-	htmlContent += "<div class=\"recipe\">";
-	htmlContent += "<div class=\"recipe-image recipe-object\"><img class=\"recipe-icon\" src=\"" + dishes[i].image + "\"></div>";
-	htmlContent += "<div class=\"recipe-info recipe-object\">";
-	htmlContent += "<h2>" + dishes[i].name + "</h2>";
-	htmlContent += "<p>" + dishes[i].info + "</p>";
-	htmlContent += "</div>";
-	htmlContent += "<div class=\"recipe-preperation recipe-object\">"
-	htmlContent += "<h2>PREPERATION</h2>"
-	htmlContent += "<p>" + dishes[i].description + "</p>"
-	htmlContent += "</div>";
-	htmlContent += "</div>";
-    
-}
-    container.append(htmlContent);
+var RecipeView = function(container, model) {
+
+    var updateHTML = function () {
+        container.empty();
+        var htmlContent = "";
+        var dishes = model.getFullMenu();
+        for (var i = 0; i < dishes.length; i++) {
+            htmlContent += "<div class=\"recipe\">";
+            htmlContent += "<div class=\"recipe-image recipe-object\"><img class=\"recipe-icon\" src=\"" + dishes[i].image + "\"></div>";
+            htmlContent += "<div class=\"recipe-info recipe-object\">";
+            htmlContent += "<h2>" + dishes[i].name + "</h2>";
+            htmlContent += "<p>" + dishes[i].info + "</p>";
+            htmlContent += "</div>";
+            htmlContent += "<div class=\"recipe-preperation recipe-object\">"
+            htmlContent += "<h2>PREPERATION</h2>"
+            htmlContent += "<p>" + dishes[i].description + "</p>"
+            htmlContent += "</div>";
+            htmlContent += "</div>";
+            container.append(htmlContent);
+        }
+
+    }
+
+    updateHTML();
+
+    var update = function(model, changeDetails) {
+        switch(changeDetails) {
+            case "added_dish_to_menu":
+                updateHTML();
+                break;
+            default:
+                break;
+        }
+    }
+    model.addObserver(update);
 }
