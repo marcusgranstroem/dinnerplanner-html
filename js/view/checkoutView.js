09f04325bf3d7f24c1cd57034db743a5794588f0
Marcus Granström <mgranst@kth.se>
2018-02-20 12:05:19 +0100

    Everything is broken and life is over.

--- js/view/checkoutView.js
+++ js/view/checkoutView.js
@@ -11,30 +11,30 @@ var CheckoutView = function (container, model) {
     htmlContent += "<div>";
     htmlContent += "<button id=\"print-full-recipe\" class=\"button button2\">Print Full Recipe</button>";
     htmlContent += "</div>";
-
+    
     container.append(htmlContent);
-
+    
     var updateHTML = function() {
         container.find('#checkout-items').empty();
         var htmlContent = "";
-
+	
         var totalPrice = 0;
-
+	
         // Generate content from model
         model.getFullMenu().forEach(function(dish) {
-    	htmlContent += "<div class=\"checkout-object\">";
-    	htmlContent += "<div class=\"dish-object\">";
-    	htmlContent += "<img src=\"" + dish.image + "\">";
-    	htmlContent += "<p class=\"dish-object-text\">" + dish.name +"</p>";
-    	htmlContent += "</div>";
-    	htmlContent += "<p id=\"dish-price-text\">" + model.getDishPrice(dish.id) + " SEK</p>";
-    	htmlContent += "</div>";
-    	totalPrice += model.getDishPrice(dish.id);
+    	    htmlContent += "<div class=\"checkout-object\">";
+    	    htmlContent += "<div class=\"dish-object\">";
+    	    htmlContent += "<img src=\"" + dish.image + "\">";
+    	    htmlContent += "<p class=\"dish-object-text\">" + dish.name +"</p>";
+    	    htmlContent += "</div>";
+    	    htmlContent += "<p id=\"dish-price-text\">" + model.getDishPrice(dish.id) + " SEK</p>";
+    	    htmlContent += "</div>";
+    	    totalPrice += model.getDishPrice(dish.id);
         });
-
+	
         htmlContent += "<hr id=\"vertical-line\">";
         htmlContent += "<div id=\"price-tag\"><p id=\"total-price-text\">Total: </p><p id=\"total-price-number\">" + totalPrice + " SEK</p></div>";
-
+	
         container.find("#checkout-items").append(htmlContent);
     }
 
