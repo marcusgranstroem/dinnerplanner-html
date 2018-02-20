265b091f58b67780b5a4844d7747ce115348752c
Marcus Granström <mgranst@kth.se>
2018-02-20 15:51:29 +0100

    Removed key.

--- js/model/dinnerModel.js
+++ js/model/dinnerModel.js
@@ -99,7 +99,7 @@ var DinnerModel = function() {
 	$.ajax( {
 	    url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search',
 	    headers: {
-		'X-Mashape-Key': '***REMOVED***'
+		'X-Mashape-Key': ''
 		
 	    }
 	}
