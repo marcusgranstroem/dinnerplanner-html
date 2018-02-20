e356305e0c47b6f349c35a51175160960cc28522
Marcus Granström <mgranst@kth.se>
2018-02-20 11:30:40 +0100

    Fixed side view.

--- js/view/sideView.js
+++ js/view/sideView.js
@@ -1,25 +1,8 @@
 var SideView = function(container, model) {
 
-    var htmlContent = "";
-    htmlContent += "<div id=\"dinner-collapse\" class=\"no-wrap\">";
-    htmlContent += "<h2 class=\"text-margin-1\">My dinner</h2>";
-    htmlContent += "<button id=\"collapse-button\" class=\"button button2 small-button\">Show</button>";
-    htmlContent += "</div>";
-    htmlContent += "<div id=\"people-input\" class=\"no-wrap\">";
-    htmlContent += "<p id=\"people-text\" class=\"text-margin-1\">People: " + model.getNumberOfGuests() + "</p>";
-    htmlContent += "<button id=\"increment-guests-btn\" class=\"button button2 small-button\">+</button>";
-    htmlContent += "<button id=\"decrement-guests-btn\" class=\"button button2 small-button\">-</button>";
-    htmlContent += "<p id=\"total-cost2\">SEK 0</p>";
-    htmlContent += "</div>";
-    htmlContent += "<div id=\"menu-content\">";
-    htmlContent += "<div class=\"dish-banner\">";
-    htmlContent += "<p class=\"dish-banner-text\">Dish name </p>";
-    htmlContent += "<p class=\"dish-banner-text\">Cost </p>";
-    htmlContent += "</div>";
-    htmlContent += "<div id=\"dishes-side-view\"></div>";
-    htmlContent += "<p id=\"total-cost\">SEK 0 </p>";
-    htmlContent += "<button id=\"confirm-button\" class=\"button button2 small-button\">Confirm Dinner </button>";
-    htmlContent += "</div>";
+     
+    var content = "People: " + model.getNumberOfGuests();
+    container.find("#people-text").html(content);
 
     this.getCollapseBtn = function() {
         return container.find('#collapse-button').get(0);
@@ -34,7 +17,6 @@ var SideView = function(container, model) {
         return container.find('#confirm-button').get(0);
     }
 
-    container.append(htmlContent);
 
     var update = function(model, changeDetails) {
 	switch(changeDetails) {
